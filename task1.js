69e9ad130c48516bb05dd473a9a97bf940a3303c
Reggie <marcus>
2023-04-04 13:18:58 +0200

    beta gamma

---
+++ task1.js
@@ -0,0 +1,18 @@
+
+
+function highlightText() {
+  document.getElementById("highlight").style.color = "blue";
+}
+
+
+  br
+// Alternate beteeen two images
+
+// Mouseover and Mouseout
+
+  function setNewImage(){
+    document.getElementById("img1").src = "./Images/img2.jpg";
+  }
+  function setOldImage(){
+    document.getElementById("img2").src = "./Images/img1.jpg";
+  }
